0a5142f289c0511e79dd77e9ad3c4c15b0c6fb27
Tomoyuki Fujii <mjr01300828@gmail.com>
2025-07-22 22:02:58 +0900

    Set up the basic front end structure

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-3xl font-bold text-center text-blue-600 mt-10">
+      Hello, Tailwind + Next.js!
+    </div>
+  );
+}
