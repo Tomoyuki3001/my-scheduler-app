4d4473e79517dffa22d4e5fd9afb0017eff9dcfe
Tomoyuki Fujii <mjr01300828@gmail.com>
2025-07-26 20:59:24 +0900

    Created the event API route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,8 +12,10 @@ app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI || "")
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+console.log("Connected to DB:", mongoose.connection.name);
 
 app.get("/api/ping", (_req, res) => {
   res.send("server is running");
